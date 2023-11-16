e4936c926bc808ec71f611847ccdda9840b3b69f
AlessandroCiaccio <146941441+AlessandroCiaccio@users.noreply.github.com>
2023-11-16 14:50:54 +0100

    Updated Main

--- Main.java
+++ Main.java
@@ -1,16 +1,17 @@
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
         OffsetDateTime data = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+        String meseItaliano = data.format(DateTimeFormatter.ofPattern("MMMM").withLocale(Locale.ITALY));
+        String giornoItaliano=data.format(DateTimeFormatter.ofPattern("EEEE").withLocale(Locale.ITALY));
         int year = data.getYear();
-        int month = data.getMonthValue();
         int day = data.getDayOfMonth();
-        String dayWeek = String.valueOf(data.getDayOfWeek());
         System.out.println("The year is " + year +
-                "\nThe month is " + month +
+                "\nThe month is " + meseItaliano +
                 "\nThe day of the month is " + day +
-                "\nThe day of the week is " + dayWeek.toLowerCase());
+                "\nThe day of the week is " + giornoItaliano);
     }
 }
